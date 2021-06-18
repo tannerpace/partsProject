f43464d587ebeb4d8797d55d944a3739516654de
TannerB <tannerpace@gmail.com>
2021-06-18 09:58:23 -0400

    changed userid to userId, partNumber 100  to 16

--- sqlScript.sql
+++ sqlScript.sql
@@ -1,11 +1,10 @@
 DROP SCHEMA if exists parts;
-
 CREATE SCHEMA `parts`;
 
 CREATE TABLE `parts`.`users` (
-id int NOT NULL unique AUTO_INCREMENT ,
- firstName varchar(33) NOT NULL,
-  lastName varchar(33) NOT NULL,
+  id int NOT NULL unique AUTO_INCREMENT,
+  firstName varchar(30) NOT NULL,
+  lastName varchar(30) NOT NULL,
   email varchar(230) unique NOT NULL,
 
   admin boolean default 0,
@@ -13,40 +12,47 @@ id int NOT NULL unique AUTO_INCREMENT ,
 );
 
 CREATE TABLE `parts`.`catalog` (
-    partNumber varchar(33) NOT NULL,
-    color varchar(100) NOT NULL,
+    partNumber varchar(16) NOT NULL,
+    color varchar(30) NOT NULL,
     partName varchar(100) NOT NULL,
     price  decimal NOT NULL,
-  primary key(partNumber)
+    primary key(partNumber)
 );
 
 CREATE TABLE `parts`.`cartItems` (
     id int NOT NULL AUTO_INCREMENT ,
-    userid int NOT NULL,
-    partNumber varchar(33) NOT NULL,
-   quantity int,
+    userId int NOT NULL,
+    partNumber varchar(16) NOT NULL,
+    quantity int,
     primary key(id),
-    FOREIGN KEY (userid) REFERENCES users(id),
+    FOREIGN KEY (userId) REFERENCES users(id),
     FOREIGN KEY (partNumber) REFERENCES catalog(partNumber)
       );
 
-CREATE TABLE `parts`.`orderItems` (
+CREATE TABLE `parts`.`orderedItems` (
     id int NOT NULL  ,
-    partNumber varchar(33),
+    partNumber varchar(16),
     quantity int NOT NULL,
-    transactionID int NOT NULL AUTO_INCREMENT ,
-    primary key(transactionID),
-    FOREIGN KEY (partNumber) REFERENCES catalog(partNumber)
+    transactionId int NOT NULL AUTO_INCREMENT ,
+    primary key(id),
+    FOREIGN KEY (partNumber) REFERENCES catalog(partNumber),
+    FOREIGN KEY (transactionId) REFERENCES pastOrders(id)
 );
 
 CREATE TABLE `parts`.`pastOrders` (
     id int NOT NULL,
-    userid int NOT NULL,
-     totalPrice decimal NOT NULL,
-       datePlaced Datetime not null DEFAULT (current_date()),
+    userId int NOT NULL,
+    totalPrice decimal NOT NULL,
+    datePlaced Datetime not null DEFAULT (current_date()),
      
-   
-
-     primary key (id),
-         FOREIGN KEY (userid) REFERENCES users(id)
+    primary key (id),
+    FOREIGN KEY (userId) REFERENCES users(id)
 );
+
+
+
+
+
+
+
+
