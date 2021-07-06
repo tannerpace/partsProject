508e125c0f4b0c639dabed1b97c12c99d55ac595
TannerB <tannerpace@gmail.com>
2021-07-06 10:28:56 -0400

    encrypted password edit user by id in rest client

--- partsProject/src/servers/partsServer/app/controllers/partStore.controller.js
+++ partsProject/src/servers/partsServer/app/controllers/partStore.controller.js
@@ -1,50 +1,103 @@
 const bcrypt = require("bcrypt");
 const { password } = require("../config/db.config");
+
 const saltRounds = 10;
 const db = require("../models/index");
 const User = require("../models/user.model.js");
 
 
-exports.welcome =  (req, res) => {
-    console.log("Parts Store is Open!")
-  
-    res.json({
-      messages: `Welcome to the parts application.`
-    });
+exports.welcome = (req, res) => {
+  console.log("Parts Store is Open!")
+
+  res.json({
+    messages: `Welcome to the parts application.`
+  });
 
 
-  };
-  
-  
- exports.createUser = (req, res) => {
-    console.log(req.body);
-  
-    const firstName = req.body.firstName;
-    const lastName = req.body.lastName;
-    const password = req.body.password;
-    const email = req.body.email;
-  
-    // const encryptedPassword = await bcrypt.hash(req.body.password, saltRounds);
-  
-    let query =
-      "INSERT INTO users (firstName, lastName, password, email) VALUES (?,?,?,?);";
-  
-    db.query(
-      query,
-      [ firstName, lastName, password, email],
-      (err, results) => {
-        if (err) {
-          if (err.code == "ER_DUP_ENTRY") {
-            res.status(409).send({ message: "user already exists!" });
-          }
-  
-          console.error(err);
-          res.status(500).send();
-          return;
-        } else {
-          console.log(results);
-          res.send("make user");
+};
+
+
+exports.createUser = async (req, res) => {
+  console.log(req.body);
+
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const encryptedPassword = await bcrypt.hash(req.body.password, saltRounds);
+  const email = req.body.email;
+
+  // const encryptedPassword = await bcrypt.hash(req.body.password, saltRounds);
+
+  let query =
+    "INSERT INTO users (firstName, lastName, password, email) VALUES (?,?,?,?);";
+
+  db.query(
+    query,
+    [firstName, lastName, encryptedPassword, email],
+    (err, results) => {
+      if (err) {
+        if (err.code == "ER_DUP_ENTRY") {
+          res.status(409).send({ message: "user already exists!" });
         }
+
+        console.error(err);
+        res.status(500).send();
+        return;
+      } else {
+        console.log(results);
+        res.send("make user");
       }
-    );
-  };
+    }
+  );
+};
+
+exports.editUserInfo = (req, res) => {
+  let id = req.params.id;
+
+  //******** WARNING: do not update userName in this query? ********
+  let firstName = req.body.firstName;
+  let lastName = req.body.lastName;
+  let email = req.body.email;
+
+
+  let query =
+    "UPDATE users \
+        SET firstName = ?, lastName = ?, email = ? \
+        WHERE id = ?;";
+
+  db.query(
+    query,
+    [firstName, lastName, email, id],
+    (err, results) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send();
+        return;
+      } else {
+
+        res.send(results[0]);
+      }
+    }
+  );
+};
+
+exports.getUserEmail = (req, res) => {
+  let email = req.params.email;
+
+  // select user by userName
+  let query = "SELECT * FROM users where email = ?;";
+
+  db.query(query, [email], (err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send();
+      return;
+    } else {
+      if (results.length == 0) {
+        //no user found
+        res.status(404).send({ message: "user not found" });
+        return;
+      }
+      res.send(results[0]);
+    }
+  });
+};
--- partsProject/src/servers/partsServer/app/routes/parts.routes.js
+++ partsProject/src/servers/partsServer/app/routes/parts.routes.js
@@ -1,7 +1,10 @@
 module.exports = app => {
-    const partStore = require("../controllers/partStore.controller");
+    const serverFunction = require("../controllers/partStore.controller");
 
-    app.get("/", partStore.welcome);
-    app.put("/",partStore.createUser);
+    app.get("/", serverFunction.welcome);
+    app.get("/user/:email", serverFunction.getUserEmail);
+    app.post("/user",serverFunction.createUser);
+    app.put("/user/:id",serverFunction.editUserInfo)
+    
     
 }
