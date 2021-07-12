919230217d734010ec72c4b4999a755034aee495
TannerB <tannerpace@gmail.com>
2021-07-12 18:15:03 -0400

    addItem .subscribe to addItem productsService

--- partsProject/src/app/components/parts-list/parts-list.component.ts
+++ partsProject/src/app/components/parts-list/parts-list.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CartService } from 'src/app/cart.service';
 import { Product } from 'src/app/models/product.model';
+import { User } from 'src/app/models/user.model';
+import { ProductService } from 'src/app/services/product.service';
+import { UserServiceService } from 'src/app/services/user-service.service';
 
 @Component({
   selector: 'app-parts-list',
@@ -8,12 +13,34 @@ import { Product } from 'src/app/models/product.model';
 })
 export class PartsListComponent implements OnInit {
 
-  @Input() products: Array<Product>;
-  product:Product;
+  @Input() products: Product[];
+  activeUser: User;
 
-  constructor() { }
+
+  constructor(private userService: UserServiceService,
+    private productsService: ProductService,
+    private cartService: CartService) { }
 
   ngOnInit(): void {
+
+    this.activeUser = this.userService.getActiveUser();
+
+    this.productsService.getAllParts()
+      .subscribe(data => {
+        this.products = data as Product[];
+      }, err => {
+        console.error(err)
+
+      })
+  }
+
+  addItem(product: Product) {
+    this.productsService.addItem(this.activeUser.id, product.partNumber)
+      .subscribe(data => {
+        console.log(data)
+      }, err => {
+        console.error("ERROR:", err)
+      });
   }
 
 }
