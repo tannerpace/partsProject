56e508cff3b28a036b4f01448ddf5cb6ec13b475
TannerB <tannerpace@gmail.com>
2021-07-14 17:40:28 -0400

    added pastOrders Component

---
+++ partsProject/src/app/components/past-orders/past-orders.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { CheckoutService } from 'src/app/services/checkout.service';
+import { UserServiceService } from 'src/app/services/user-service.service';
+
+@Component({
+  selector: 'app-past-orders',
+  templateUrl: './past-orders.component.html',
+  styleUrls: ['./past-orders.component.css']
+})
+export class PastOrdersComponent implements OnInit {
+  activeUser: any;
+  orders: any;
+
+  constructor(private userService: UserServiceService) { }
+
+  ngOnInit(): void {
+    this.activeUser = this.userService.getActiveUser();
+    this.userService.getPastOrders(this.activeUser.id)
+      .subscribe(data => {
+        this.orders = data;
+      }, err => {
+        console.error(err)
+
+      })
+  }
+
+}
