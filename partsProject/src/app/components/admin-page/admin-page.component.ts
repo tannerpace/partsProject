fa1ab19970c903fbc6a44c88330700021942f19f
TannerB <tannerpace@gmail.com>
2021-07-29 08:56:41 -0400

    admin page , an admin can add and remove products.

---
+++ partsProject/src/app/components/admin-page/admin-page.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CartService } from 'src/app/cart.service';
+import { Product } from 'src/app/models/product.model';
+import { User } from 'src/app/models/user.model';
+import { CheckoutService } from 'src/app/services/checkout.service';
+import { ProductService } from 'src/app/services/product.service';
+import { UserServiceService } from 'src/app/services/user-service.service';
+
+@Component({
+  selector: 'app-admin-page',
+  templateUrl: './admin-page.component.html',
+  styleUrls: ['./admin-page.component.css']
+})
+export class AdminPageComponent implements OnInit {
+  activeUser: any;
+  product: Product;
+
+  constructor(private productService: ProductService,
+    private userService: UserServiceService,
+
+    private router: Router,
+    public cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.activeUser = this.userService.getActiveUser();
+    this.product = {
+      partNumber: '',
+      primaryVendor: '',
+      color: '',
+      partName: '',
+      price: '',
+      catagory: '',
+      img: ''
+    }
+    this.product = new Product(this.product);
+
+  }
+
+  onSubmit(form: NgForm) {
+    console.log("adding a prt")
+    this.productService.addNewPart(this.product)
+      .subscribe(
+        data => {
+          console.log("New Part Created Successfully")
+
+        },
+        error => {
+          console.error("ERROR creating user: ", error)
+        }
+      )
+  }
+}
